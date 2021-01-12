ff96ae2d7a078a02de4f5b0cd5b0a863c1069aec
dev-geeyong <dev.geeyong@gmail.com>
2021-01-13 01:03:48 +0900

    customCell add Like button

--- ODayOWhite/MessageCell.swift
+++ ODayOWhite/MessageCell.swift
@@ -12,14 +12,27 @@ class MessageCell: UITableViewCell {
     @IBOutlet var messageTextLabel: UILabel!
     @IBOutlet var messageSenderLabel: UILabel!
     
-    @IBOutlet var messageCountLike: UILabel!
-    
+
+    var bRec:Bool = true
     @IBOutlet var messageLikeButton: UIButton!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    //heart
+    
+    @IBAction func touchUpLikeButton(_ sender: UIButton) {//heart.fill
+        bRec = !bRec
+            if bRec {
+                //messageLikeButton.setImage(UIImage(named: "heart"), for: .normal)
+                messageLikeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+                print(messageTextLabel.text)
+            } else {
+                messageLikeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            }
+        
+        
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
